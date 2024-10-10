251a2401d5a05006f6df862f00a5bb6305f71f4e
Yaksh Git <chudasamayaksh77@gmail.com>
2024-10-10 16:56:29 +0530

    Learned about loops

---
+++ Day_3/Loops/script.js
@@ -0,0 +1,36 @@
+// //* Loops and Strings
+
+// //* for-of loop
+
+// let arr = [1, 3, 5, 7, 8];
+// for (let value of arr) {
+//   console.log(value);
+// }
+
+// let obj = {
+//   name: "Yaksh",
+//   age: 23,
+// };
+// for (let value in obj) {
+//   console.log("Key :", value, "Value :", obj[value]);
+// }
+
+// //* Practice-1
+
+// let num = 100;
+// for (let i = 0; i <= num; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
+
+//* Practice-2
+
+// let originalNum = 5;
+// let gussedNum = prompt("Guess the number :");
+// while (gussedNum != originalNum) {
+//   gussedNum = prompt("Guess the next number :");
+// }
+// alert("You have guessed correct number");
+
+
