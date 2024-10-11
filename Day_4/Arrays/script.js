346565b8451e8a46c1d038eeec00dd46c7bdf228
Yaksh Git <chudasamayaksh77@gmail.com>
2024-10-11 18:46:52 +0530

    Learned about arrays

---
+++ Day_4/Arrays/script.js
@@ -0,0 +1,36 @@
+//* Strings
+
+// let str = "Yaksh Chudasama";
+// console.log(str);
+
+//* Template literals
+
+// const obj = {
+//   item: "pen",
+//   price: 10,
+// };
+// let specialString = `The cost of ${obj.item} is ${obj.price} rupees`;
+// console.log(specialString);
+
+//* String Methods
+
+// let someString = "Yaksh Chudasama";
+// let newString = "UmeshBhai";
+// console.log(someString.slice(0, 5));
+// console.log(someString.concat(" " + newString));
+// console.log(someString + newString);
+// console.log(someString.toUpperCase());
+// console.log(someString.toLowerCase());
+// console.log(someString.replace("Yaksh", "Harsh"));
+
+// let newString1 = "Hellololo";
+// console.log(newString1.replaceAll("lo", "p"));
+
+//* Practice-1
+
+let userString = prompt("Enter you full name :");
+let userStringLenght = userString.length;
+let username = `@${userString}${userStringLenght}`;
+console.log(username);
+
+
