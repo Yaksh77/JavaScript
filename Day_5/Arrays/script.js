cfd719694ea9c108030a6b9f603ce1b5d1db14e3
Yaksh Git <chudasamayaksh77@gmail.com>
2024-10-12 19:28:37 +0530

    Learned about Arrays

---
+++ Day_5/Arrays/script.js
@@ -0,0 +1,69 @@
+//* Arrays
+
+// const arr = ["Yaksh", "Harsh", "Harshil", "Yash", "Kandrap"];
+// for (let value of arr) {
+//   console.log(value);
+// }
+
+// //* Practice-1
+
+// const marks = [89, 99, 67, 78, 88];
+// let total = 0;
+// for (let mark of marks) {
+//   total += mark;
+// }
+// console.log("The average mark is :", total / marks.length);
+
+// //* Practice-2
+
+// const prices = [250, 645, 300, 900, 50];
+// for (let i in prices) {
+//   prices[i] = prices[i] - prices[i] * 0.1;
+// }
+// console.log(prices);
+
+// //* Array methods()
+
+// let names = ["Harsh", "Yaksh", "Yash", "Harshil", "Om", "Kandrap"];
+// let heroes = ["Shaktiman", "Krishna", "Hanuman", "Shivji"];
+// names.push("Rudra", "Ram", "Rohan");
+// console.log(names);
+// let ele = names.pop();
+// console.log(names);
+// console.log(names.toString());
+// let newArr = names.concat(heroes);
+// console.log(newArr);
+// names.unshift("Bhim", "Arjun", "karn");
+// console.log(names);
+// let ele1 = names.shift();
+// console.log(names);
+
+// let numeric = [1, 2, 3, 4, 5, 6, 7];
+// // console.log(numeric.splice(2, 2, 101, 102));
+// // console.log(numeric);
+
+// //* Only add element
+// // numeric.splice(4, 0, 104);
+// // console.log(numeric);
+
+// //* Only Delete element
+// // numeric.splice(2, 1);
+// // console.log(numeric);
+
+// //* Replace element
+// // numeric.splice(1, 2, 102, 103);
+// // console.log(numeric);
+
+// //* Practice-1
+
+// let companies = ["Bloomberg", "Microsoft", "Uber", "Google", "IBM", "Netflix"];
+// // let firstCompany = companies.shift();
+// // console.log(companies);
+
+// // companies.splice(2, 1, "Ola");
+// // console.log(companies);
+
+// companies.push("Amazon");
+// console.log(companies);
+
+
